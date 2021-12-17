3409c78298d8bcb68e34cc6a0a16c4ff015e2e7a
Procepss <arti_rnd@mail.ru>
2021-12-17 13:38:20 +0300

    fix2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-function sayArtur () {
-    console.log('Привет, Артур')
-}  
+import _ from 'lodash';
+
+import './styles/style.scss';
+
+import Svg from './images/Telega.svg';
+
+import {a} from './a.js';
+
+
+
+// let x;
+
+// const a = function() {
+// 	x = 1;
+// 	return console.log(x);
+// };
+
+
+
+// function component() {
+// 	const element = document.createElement('div');
+// 	const btn = document.createElement('button');
+
+// 	element.innerHTML = _.join(["Hello", "webpack"], ' ');
+// 	element.classList.add('hello');
+
+// 	btn.innerHTML = 'Click me and check the console!';
+// 	// btn.onclick = d;
+
+// 	element.appendChild(btn);
+
+// 	const myImg = new Image();
+
+// 	myImg.src = Svg;
+
+// 	element.appendChild(myImg);
+
+
+// 	return element;
+// }
+
+// document.body.appendChild(component());
+
+
+a();
